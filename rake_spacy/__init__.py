27cff0657c2ac86c7134f07fa0fbde6dbb145d92
Andrew Ross <andrew.ross.mail@gmail.com>
2020-09-10 02:19:55 -0400

    Update __init__.py

--- rake_spacy/__init__.py
+++ rake_spacy/__init__.py
@@ -1,4 +1,10 @@
+from . import aggregators  # noqa: F401
+from . import cog  # noqa: F401
+from . import mappers  # noqa: F401
+from . import phrasers  # noqa: F401
 from . import rake  # noqa: F401
+from . import scorers  # noqa: F401
+from . import stop_tokens  # noqa: F401
 from .__version__ import __version__  # noqa: F401
 from .rake import Metric  # noqa: F401
 from .rake import Rake  # noqa: F401
